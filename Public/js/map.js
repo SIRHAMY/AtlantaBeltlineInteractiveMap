7687fdd68b3ce5c467b9ead3815d1684f3befb40
Hamilton Greene <hamilton.greene@gmail.com>
2016-02-29 10:19:37 -0500

    Got map to show up

--- Public/js/map.js
+++ Public/js/map.js
@@ -1,3 +1,16 @@
 //var map = L.map('map').setView([51.505, -0.09], 13);
 var map = L.map('map');
-L.geoJson(geojsonFeature).addTo(map);
+
+var geojsonFeature; // = new L.GeoJson.AJAX("./Data/Maps/NPUs.json");
+
+$.ajax({
+dataType: "json",
+url: "./Data/Maps/NPUs.json",
+success: function(data) {
+    $(data.features).each(function(key, data) {
+        geojsonFeature.addData(data);
+    });
+}
+}).error(function() {});
+
+geojsonFeature.addTo(map);
